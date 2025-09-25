f5a4387f3e08a6341fb54e5407ebc6b1b88042ac
Nathan Miller <nami2227@colorado.edu>
2025-09-25 07:31:47 -0600

    FEAT: Added penalty formulation of nodal contact

---
+++ include/nodalkernels/CylindricalSurfaceNodalContact.h
@@ -0,0 +1,53 @@
+#ifndef CYLINDRICALSURFACEDIRICHLETBC_H
+#define CYLINDRICALSURFACEDIRICHLETBC_H
+
+#include<NodalKernel.h>
+
+class CylindricalSurfaceNodalContact : public NodalKernel
+{
+
+    public:
+
+        CylindricalSurfaceNodalContact( const InputParameters &parameters );
+
+        static InputParameters validParams();
+
+
+    protected:
+
+        virtual bool isOverclosed() const;
+
+        virtual Real computeQpResidual() override;
+
+        virtual Real computeQpJacobian() override;
+
+        virtual Real computeQpOffDiagJacobian( const unsigned int jvar_num ) override;
+
+        std::vector<unsigned int> _displacements;
+
+    private:
+
+        const VariableValue & _u_dot; //!< The time derivative of the degree of freedom
+
+        const VariableValue & _du_dot_du; //!< The Jacobian of the time derivative of the degree of freedom w.r.t. the degree of freedom
+
+        Point _center; //!< A point on the cylindrical axis prior to motion
+        Real _radius; //!< Radius of the cylindrical surface
+        Real _velocity; //!< Magnitude of velocity of the plane along its normal
+        RealVectorValue _axis; //!< Axis direction of the cylinder
+        RealVectorValue _normal; //!< Direction of motion of surface
+        RealVectorValue _disp_dir; //!< Direction associated with the Dirichlet BC degree of freedom
+
+        bool _use_sector; //!< Limit BC to a sector between angle_min and angle_max
+        Real _angle_min; //!< Minimum angle in radians for active sector
+        Real _angle_max; //!< Maximum angle in radians for active sector
+        bool _invert_displacement; //!< Reverse the sign of the displacement correction
+        Real _penalty_parameter; //!< The penalty parameter applied to the overclosed distance
+
+        bool _beta; //!< A damping coefficient to help with stability 
+
+        bool inAngularRange( const Point & pt ) const;
+        Real computeSignedDistanceToSurface( const Point & pt ) const;
+};
+
+#endif
